cc2c19ed7ae1f8cfa7e5995a2ae15aade019149b
Luke Bunselmeyer <lbunselmeyer@switchfly.com>
2013-01-05 20:14:11 -0800

    Added unit tests for services and key event directives

---
+++ test/flash-test.js
@@ -0,0 +1,37 @@
+describe('FlashService', function () {
+    'use strict';
+
+    var _flash;
+
+    beforeEach(module('angular-common.flash-service'));
+
+    beforeEach(inject(function (flash) {
+        _flash = flash;
+    }));
+
+    it('it should send flash messages to subscribers', function () {
+
+        var subscriber1 = jasmine.createSpy('subscriber1');
+        var subscriber2 = jasmine.createSpy('subscriber2');
+
+        _flash.subscribe(subscriber1);
+        _flash.subscribe(subscriber2);
+
+        _flash.error = ':error-message';
+        _flash.warn = ':warn-message';
+        _flash.info = ':info-message';
+        _flash.success = ':success-message';
+
+        expect(subscriber1).toHaveBeenCalledWith(':error-message', 'error');
+        expect(subscriber1).toHaveBeenCalledWith(':warn-message', 'warn');
+        expect(subscriber1).toHaveBeenCalledWith(':info-message', 'info');
+        expect(subscriber1).toHaveBeenCalledWith(':success-message', 'success');
+
+        expect(subscriber2).toHaveBeenCalledWith(':error-message', 'error');
+        expect(subscriber2).toHaveBeenCalledWith(':warn-message', 'warn');
+        expect(subscriber2).toHaveBeenCalledWith(':info-message', 'info');
+        expect(subscriber2).toHaveBeenCalledWith(':success-message', 'success');
+
+    });
+
+});
---
+++ test/key-event-directives-test.js
@@ -0,0 +1,78 @@
+describe('FlashService', function () {
+    'use strict';
+
+    var scope, $sandbox, $compile, $timeout;
+
+    beforeEach(module('angular-common.key-event-directives'));
+
+    beforeEach(inject(function ($injector, $rootScope, _$compile_, _$timeout_) {
+        scope = $rootScope;
+        $compile = _$compile_;
+        $timeout = _$timeout_;
+
+        $sandbox = $('<div id="sandbox"></div>').appendTo($('body'));
+        scope.model = {};
+    }));
+
+    afterEach(function () {
+        $sandbox.remove();
+        scope.$destroy();
+    });
+
+    function compileDirective(template) {
+        template = $(template).appendTo($sandbox);
+        return $compile(template)(scope);
+    }
+
+    describe('key-enter directive', function () {
+
+        it('should run the passed expression on enter keydown event', function () {
+            scope.onEnter = jasmine.createSpy('onEnter');
+
+            var elm = compileDirective('<div key-enter="onEnter()"></div>');
+
+            var keydownEnter = jQuery.Event("keydown");
+            keydownEnter.which = 13;
+            elm.trigger(keydownEnter);
+
+            var keypressEnter = jQuery.Event("keypress");
+            keypressEnter.which = 13;
+            elm.trigger(keypressEnter);
+
+            var e = jQuery.Event("keydown");
+            e.which = 50;
+            elm.trigger(e);
+
+            expect(scope.onEnter).toHaveBeenCalled();
+            expect(scope.onEnter.callCount).toBe(1);
+        });
+
+    });
+
+    describe('key-up-arrow directive', function () {
+
+        it('should run the passed expression on up-arrow keyup event', function () {
+            scope.onUp = jasmine.createSpy('onUp');
+
+            var elm = compileDirective('<div key-up-arrow="onUp()"></div>');
+
+            var keyupUpArrow = jQuery.Event("keyup");
+            keyupUpArrow.which = 38;
+            elm.trigger(keyupUpArrow);
+
+            var keypressUpArrow = jQuery.Event("keypress");
+            keypressUpArrow.which = 38;
+            elm.trigger(keypressUpArrow);
+
+            var e = jQuery.Event("keydown");
+            e.which = 50;
+            elm.trigger(e);
+
+            expect(scope.onUp).toHaveBeenCalled();
+            expect(scope.onUp.callCount).toBe(1);
+        });
+
+    });
+
+
+});
